2aaf1859f3cd3befbcfab961b92b0c64cd4eec44
Антон Клинцов <92170048@study.bsuir.by>
2020-11-28 11:36:05 +0300

    set_additions

--- PPiIS-3/main.cpp
+++ PPiIS-3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>;
 #include <cstring>;
+#include <typeinfo>;
 
 using namespace std;
 
@@ -36,7 +37,7 @@ class carriage
 {
 public:
 
-	virtual void loading(const int& value) = 0;
+	virtual void cloading(const int& value) = 0;
 
 protected:
 
@@ -56,7 +57,7 @@ public:
 		_passengers._number = number;
 	}
 
-	void loading(const int& value) override
+	void cloading(const int& value) override
 	{
 		_passengers._number = value;
 	}
@@ -79,7 +80,7 @@ public:
 		_cargo._number = number;
 	}
 
-	void loading(const int& value) override
+	void cloading(const int& value) override
 	{
 		_cargo._number = value;
 	}
@@ -93,8 +94,113 @@ protected:
 class train
 {
 public:
+	virtual int tloading(const int& value) = 0;
 protected:
 };
+
+class PasTrain : train
+{
+public:
+	PasTrain()
+	{
+		_carriages = new PassengerCarriage[20];
+	}
+
+	int tloading(const int& value) override
+	{
+		int t = value;
+		for (int i = 0; i < 20; i++)
+		{			
+			if (t >= 0 || t < 50)
+				_carriages[i].cloading(t);
+			else if (t >= 50)
+			{
+				t -= 50;
+				_carriages[i].cloading(50);
+			}
+		}
+		return t;
+	}
+
+protected:
+	PassengerCarriage* _carriages;
+};
+
+class CarTrain : train
+{
+public:
+	CarTrain()
+	{
+		_carriages = new CargoCarriage[20];
+	}
+
+	int tloading(const int& value) override
+	{
+		int t = value;
+		for (int i = 0; i < 20; i++)
+		{
+			if (t >= 0 || t < 50)
+				_carriages[i].cloading(t);
+			else if (t >= 50)
+			{
+				t -= 50;
+				_carriages[i].cloading(50);
+			}
+		}
+		return t;
+	}
+
+protected:
+	CargoCarriage* _carriages;
+};
+
+class PasCarTrain : train
+{
+public:
+	PasCarTrain()
+	{
+		_carriages1 = new CargoCarriage[10];
+		_carriages2 = new PassengerCarriage[10];
+	}
+
+	int tloading(const int& cargo) override
+	{
+		int t = cargo;
+		for (int i = 0; i < 20; i++)
+		{
+			if (t >= 0 || t < 50)
+				_carriages1[i].cloading(t);
+			else if (t >= 50)
+			{
+				t -= 50;
+				_carriages1[i].cloading(50);
+			}
+		}
+		return t;
+	}
+
+	int addloading(const int& passengers)
+	{
+		int t = passengers;
+		for (int i = 0; i < 20; i++)
+		{
+			if (t >= 0 || t < 50)
+				_carriages2[i].cloading(t);
+			else if (t >= 50)
+			{
+				t -= 50;
+				_carriages2[i].cloading(50);
+			}
+		}
+		return t;
+	}
+
+protected:
+	CargoCarriage* _carriages1;
+	PassengerCarriage* _carriages2;
+};
+
+
 
 class station
 {
@@ -103,7 +209,6 @@ protected:
 	int _passengers;
 	int _wood;
 	int _coal;
-	int _sand;
 };
 
 
